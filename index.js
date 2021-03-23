e8c6bdf5eaf4401707ae1e47d22a56cb7fa9d829
kowsalya980113 <kowsalya980113@gmail.com>
2021-03-23 16:55:30 +0530

    transactions 23/03/21 4.55pm

--- index.js
+++ index.js
@@ -99,6 +99,12 @@ const masters = require('./routes/masters.routes');
 app.use('/masters', masters);
 
 
+const transactions = require('./routes/transactions.routes');
+
+app.use('/transactions', transactions);
+
+
+
 const reports_designer = require('./routes/reports_designer.routes');
 
 app.use('/reports_designer', reports_designer);
---
+++ sql/order_program.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `order_program` ADD `style_id` VARCHAR(255) NOT NULL AFTER `size_id`;
